002de61410dbc97dc53cf25e6e82840c1e05f81a
zulal <zulaltak@gmail.com>
2020-01-11 11:23:45 +0300

    ders-1

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_bootcamp_b1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducController:ControllerBase
+    {
+        [HttpGet("Hello")]
+        public IActionResult Hello()
+        {
+            return Ok("Hello World");
+        }
+
+    }
+}
