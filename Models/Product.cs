1217c539b62149bfa8ea934d7d8e7ad579f282e2
zulal <zulaltak@gmail.com>
2020-01-13 20:55:22 +0300

    Add dtos and model to product.

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace net_core_bootcamp_b1.Models
+{
+    public class Product // belge adimiz
+    {
+        public Guid? Id { get; set; } // rastgele string karakterler ile id aldık -> Guid bu ise yarar. ""? sor"
+        public bool IsDeleted { get; set; } // bool: true-false yapisi gibi dusun """IsDeleted sor""
+        public DateTime? MyProperty { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public double Price { get; set; }
+    }
+}
